63f5e37dd41225e8c90b3b2a79bde5e51c04e7de
caiosdf <artedetodagenteweb@gmail.com>
2021-01-11 09:37:01 -0300

    +CarouselBanner

---
+++ src/components/CarouselBanner/index.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Wrapper, Slide, Item, Dots } from './styles';
+
+export default function CarouselBanner({autoplay, source, h}){
+    const carousel = useRef();
+    const [settings, setSettings] = useState({ autoplay: false, slides: [], selected: 0, prevSelected: 0 });
+    const [dots, setDots] = useState([]);
+    
+    useEffect(() => {
+        setSettings({ ...settings, autoplay, slides: source });
+    }, []);
+
+    useEffect(() => {
+      if (!settings.autoplay) return () => null;
+      const timer = setInterval(() => {
+        const prev = settings.selected;
+        const pos = settings.selected === settings.slides.length - 1 ? 0 : settings.selected + 1;
+        setSettings({ ...settings, selected: pos, prevSelected: prev });
+      }, 5000);
+      return () => clearInterval(timer);
+    });
+
+    useEffect(() => {
+      const size = carousel.current.offsetWidth;
+      if(settings.autoplay){      
+        if (settings.selected === 0) carousel.current.scrollBy(-(size * settings.slides.length), 0);
+        else carousel.current.scrollBy(size, 0);
+      }else{
+        if(settings.prevSelected > settings.selected) carousel.current.scrollBy(-(size * (settings.prevSelected - settings.selected)), 0);
+        else if(settings.prevSelected == settings.selected) return () => null;
+        else carousel.current.scrollBy(size * (settings.selected - settings.prevSelected), 0);
+      }
+    }, [settings.selected, settings.autoplay]);
+
+    useEffect(() => {
+      let dotsList = [];
+      for(let x = 0; x <= settings.slides.length - 1; x++){
+        dotsList.push(<button onClick={() => {
+          const prev = settings.selected;
+          setSettings({ ...settings, selected: x, prevSelected: prev, autoplay: false});
+        }} key= {x}></button>);
+      }
+      setDots(dotsList);
+    }, [settings.slides, settings.selected]);
+
+      return(
+        <>
+        <Wrapper
+            h={h}
+            ref={carousel}
+            onMouseOver={() => setSettings({ ...settings, autoplay: false })}
+            onMouseLeave={() => setSettings({ ...settings, autoplay: true })}
+        >
+          {
+            settings.slides.map((slide, index) => (
+              <Slide key={index}>
+                  <Item
+                    key={slide.id}
+                    photo="https://img.freepik.com/fotos-gratis/transicao-suave-no-azul-para-o-verde_23-2147734210.jpg?size=626&ext=jpg"
+                  >
+                    <div className="text-container">
+                      <h1>{slide.title}</h1>
+                      <p>{slide.description}</p>
+                    </div>
+                    <Dots>
+                      {dots}
+                    </Dots> 
+                  </Item>
+              </Slide>
+            ))
+          }       
+        </Wrapper>
+        </>
+    
+      )
+}
+
+CarouselBanner.propTypes = {
+  source: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    photo: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+  })),
+  autoplay: PropTypes.bool,
+  h: PropTypes.number,
+};
+
+CarouselBanner.defaultProps = {
+  source: [],
+  autoplay: true,
+  h: 412,
+};
---
+++ src/components/CarouselBanner/styles.js
@@ -0,0 +1,95 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: ${({ h }) => h}px;
+  width: 100%;
+  display: flex;
+  overflow-x: auto;
+  scroll-snap-type: x mandatory;
+  -webkit-overflow-scrolling: touch;
+  scroll-behavior: smooth;
+  margin: 20px 0;
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+
+`;
+
+export const Dots = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: center;
+  button{
+    height: 16px;
+    width: 16px;
+    border-radius: 10px;
+    background-color: #585858;
+    margin: 15px 5px;
+    cursor: pointer;
+    border-style: none;
+    &:hover{
+      background-color: #E4BE2B;
+    }
+  }
+`;
+
+export const Slide = styled.ul`
+  flex: none;
+  scroll-snap-align: start;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+`;
+
+export const Item = styled.li`
+  width: 100%;
+  height: 100%;
+  background-image: ${({ photo }) => (photo ? `url(${photo})` : '')};
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+
+  .text-container{
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+    margin-bottom: 100px;
+  }
+
+  h1 {
+    font-size: 1.2em;
+    color: #fff;
+    padding: 10px 5px 10px 50px;
+    margin: 0;
+    width: calc(100% - 10px);
+    width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    z-index: 995;
+  }
+
+  p {
+    color: #fff;
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    transition: opacity .3s linear;
+    height: calc(100% - 20px);
+    padding: 10px 5px 10px 50px;
+    z-index: 999;
+  }
+
+`;
