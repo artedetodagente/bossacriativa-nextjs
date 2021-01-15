24cfd76fe2cee30033c959d44daf0c017aae0810
Renan Ferreira de Lima <renanferreiradelima98@live.com>
2021-01-15 13:05:27 -0300

    Correçao de design do card de noticias

--- src/components/CardText/styles.js
+++ src/components/CardText/styles.js
@@ -5,9 +5,9 @@ export const Card = styled.main`
 
   div {
     :last-of-type {
-      padding: 15px 20px;
+      padding: 15px 10px;
       background-color: rgb(204, 204, 204);
-      height: 150px;
+      height: 100px;
 
       p {
         margin: 0;
@@ -15,7 +15,7 @@ export const Card = styled.main`
         overflow: hidden;
         text-overflow: ellipsis;
         display: -webkit-box;
-        -webkit-line-clamp: 5;
+        -webkit-line-clamp: 3;
         -webkit-box-orient: vertical;
       }
     }
@@ -23,23 +23,23 @@ export const Card = styled.main`
 `;
 
 export const Image = styled.div`
-  height: 270px;
+  height: 300px;
   background-size: conver;
   background-position: center;
   background-repeat: no-repeat;
   background-color: #444;
-  ${({ image }) => image && `background-image: url(${image})`};
   position: relative;
+  ${({ image }) => image && `background-image: url(${image})`};
 
   h1 {
     font-size: 1.5em;
-    background-color: rgba(2, 2, 2, .6);
+    background-color: rgba(2, 2, 2, .9);
     color: #fff;
     padding: 5px 10px;
     margin: 0;
-    text-align: center;
     position: absolute;
     width: 100%;
+    height: 90px;
     bottom: 0;
     overflow: hidden;
     text-overflow: ellipsis;
@@ -48,6 +48,6 @@ export const Image = styled.div`
     -webkit-box-orient: vertical;
     z-index: 3;
     font-family: 'Barlow Condensed';
-    font-weight: 500;
+    font-weight: 600;
   }
 `;
