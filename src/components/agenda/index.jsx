10a945567a412415e518cc4f142a0aaf6a558ada
caiosdf <caiosdf15@gmail.com>
2021-01-21 18:02:38 -0300

    +toolbar na pagina de agenda

--- src/components/agenda/index.jsx
+++ src/components/agenda/index.jsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Image from 'next/image';
 import AgendaCarousel from '../AgendaCarousel';
 import {
-  WrapperCarousel,
+  WrapperCarousel, WrapperAgenda, Toolbar,
 } from './style';
 
 export default function Schedule() {
+
+  const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+  const image = require('@/images/loupe.svg');
+  const[now, setNow] = useState('Fevereiro - 2021');
+
+  useEffect(() => {
+    let date = new Date();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    setNow(`${months[month]} - ${year}`);
+  }, [])
   return (
-    <WrapperCarousel>
-      <AgendaCarousel width="49%" />
-      <AgendaCarousel width="49%" />
-    </WrapperCarousel>
+    <>
+      <WrapperCarousel>
+        <AgendaCarousel width="49%" />
+        <AgendaCarousel width="49%" />
+      </WrapperCarousel>
+      <WrapperAgenda>
+        <Toolbar>
+          <div className="months-nav">
+            <button className="arrow">◀</button>
+            <button className="arrow">▶</button>
+            <button className="months">{now}</button>
+          </div>
+          <div className="filter-container">
+            <form className="filter">
+              <input type='submit' value='a' id="search-button"/>
+              <input type='text' id='query' value='Digite aqui para buscar...' />
+            </form>
+            <button className="filter-button">Todas</button>
+            <button className="filter-button">Virtual</button>
+            <button className="filter-button">Online</button>
+            <button className="filter-button">Presencial</button>
+          </div>
+        </Toolbar>
+      </WrapperAgenda>
+    </>
   );
 }
--- src/components/agenda/style.js
+++ src/components/agenda/style.js
@@ -1,11 +1,82 @@
 import styled from 'styled-components';
 
 export const WrapperCarousel = styled.div`
-  height: ${({ height }) => height}px;
-  width: ${({ width }) => width};
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   margin: 20px 0;
 
 `;
+
+export const WrapperAgenda = styled.div`
+
+`;
+
+export const Toolbar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  font-family: 'Barlow Condensed Light';
+  font-size: medium;
+
+  button {
+    border-radius: 18px;
+    height: 35px;
+    border: solid 2px #313131;  
+    cursor: pointer;
+  }
+
+  .months-nav {
+
+    .arrow {
+      width: 70px;
+  
+      :first-of-type {
+        margin-right: 5px;
+      }
+    }
+  
+    .months {
+      margin-left: 10px;
+      background-color: #E4BE2B;
+    }
+  }
+
+  .filter-container {
+    display: flex;
+    flex-direction: row;
+
+    .filter {
+      margin-right: 20px;
+
+      #query {
+        height: 30px;
+        width: 200px;
+        border-radius: 0 15px 15px 0;
+        border: none; 
+        color: #E4E8E8;
+      }
+      #search-button {
+        height: 30px;
+        background-color: #313131;
+        border-radius: 15px 0 0 15px;
+        border: none;
+        width: 40px;
+        cursor: pointer;
+      }
+    }
+
+    .filter-button {
+      border: none;
+      background-color: rgb(230, 231, 233);
+      padding: 0 20px;
+
+      &:hover {
+        background-color: #313131;
+        color: rgb(230, 231, 233);
+      }
+    }
+  }
+
+`;
+
