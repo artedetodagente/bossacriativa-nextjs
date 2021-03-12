e5f8f24cf1293fb8ec5b4b43d3d44501cb2dc256
Eduardo Mangeli <eduardo@mangeli.com.br>
2021-03-11 22:16:08 -0300

    página de apresentações com séries - ajuste na consulta

--- src/core/mostras.js
+++ src/core/mostras.js
@@ -24,6 +24,13 @@ export async function getAll(quant, slug) {
             acf_data {
               videoUrl
             }
+            apresentacoesSeries {
+              nodes {
+                name
+                slug
+                id
+              }
+            }
           }
         }
       }
@@ -50,6 +57,13 @@ export async function getOne(slug) {
             excerpt
             acf_data {
               videoUrl
+            }
+            apresentacoesSeries {
+              nodes {
+                name
+                slug
+                id
+              }
             }
             content
           }
@@ -95,3 +109,85 @@ export async function getLast(last = 2) {
   );
   return data?.mostrasVirtuais;
 }
+
+export async function getSerie(slug, qtd = 100) {
+  const data = await fetchAPI(`
+    query ($slug : [String], $qtd : Int) {
+      apresentacoesSeries(where: {slug: $slug}) {
+        nodes {
+          id
+          name
+          slug
+          description
+          acf_data {
+            descricaoCompleta
+            imagemDestacada {
+              mediaItemUrl
+            }
+            categoria {
+              name
+              slug
+            }
+          }
+          mostrasVirtuais(last: $qtd) {
+            nodes {
+              id
+              slug
+              title
+              excerpt
+              acf_data {
+                videoUrl
+              }
+              featuredImage {
+                node {
+                    mediaItemUrl
+                }
+              }
+            }
+          }
+        }
+      }
+    }`, {
+    variables: { slug, qtd },
+  });
+  return data?.apresentacoesSeries;
+}
+
+export async function getSeries(slug, qtd = 100) {
+  const data = await fetchAPI(`
+  query ($qtd : Int) {
+    apresentacoesSeries {
+      nodes {
+        id
+        name
+        slug
+        description
+        acf_data {
+          descricaoCompleta
+          imagemDestacada {
+            mediaItemUrl
+          }
+          categoria {
+            name
+            slug
+          }
+        }
+        mostrasVirtuais(last: $qtd) {
+          nodes {
+            id
+            slug
+            title
+            excerpt
+            acf_data {
+              videoUrl
+              
+            }
+          }
+        }
+      }
+    }
+  }`, {
+    variables: { qtd },
+  });
+  return data?.apresentacoesSeries;
+};
