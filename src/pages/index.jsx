8bf2a98aec44f68de159b6c306997c0e59797bfb
Eduardo Mangeli <eduardo@mangeli.com.br>
2021-03-12 14:32:59 -0300

    mudança do texto da faixa da home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -37,11 +37,12 @@ export default function Home({
       <div className={styles.description}>
         <div area="text">
           <p>
-            No Bossa Criativa, arte, cultura e inclusão têm como palco a internet e patrimônios da
-            humanidade. São mais de 180 artistas e educadores, de várias regiões do país, em
-            apresentações, lives e oficinas de capacitação nas áreas de música, circo,
-            artes visuais, dança, teatro e gestão cultural. Mais de 200 horas de conteúdo já
-            estão no ar, com foco na diversidade e democratização da cultura.
+            No Bossa Criativa, arte, cultura e patrimônios da humanidade
+            têm como palco a internet. São mais de 180 artistas e educadores,
+            de várias regiões do país, em apresentações, oficinas de capacitação
+            e lives nas áreas de música, circo, artes visuais, dança, teatro e
+            gestão cultural. Mais de 200 horas de conteúdo já estão no ar, com
+            foco na diversidade, inclusão e democratização da cultura.
           </p>
         </div>
         <div area="action" className={styles.botaovideo}>
