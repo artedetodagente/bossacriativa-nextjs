5d567647ca8819b89eba69381b896371118fbd0d
Eduardo Mangeli <eduardo@mangeli.com.br>
2021-03-22 20:14:53 -0300

    incluindo elementos do css do wordpress no página de notícias

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -4,7 +4,9 @@ import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
 import SchoolBar from '@/components/SchoolBar';
 
-export default function Page({ menus, links, children }) {
+export default function Page({
+  menus, links, cssLink, children,
+}) {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -14,6 +16,7 @@ export default function Page({ menus, links, children }) {
       <Head>
         <title>Bossa Criativa - Arte de Toda Gente</title>
         <link rel="icon" href="/favicon.ico" />
+        { (cssLink) ? <link href={`/css/${cssLink}`} rel="stylesheet" /> : '' }
       </Head>
       <header>
         <SchoolBar data={links} />
