f8a35b7a13078530294103a62b3cde527c0e7939
Eduardo Mangeli <eduardo@mangeli.com.br>
2021-03-31 13:57:13 -0300

    incluindo descrição das imagens no lightbox #26

--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -37,6 +37,7 @@ const Galeria = ({ clickAction }) => {
                                 nodes {
                                   slug
                                   acf_galeria {
+                                    descricao
                                     imagem {
                                       mediaItemUrl
                                     }
