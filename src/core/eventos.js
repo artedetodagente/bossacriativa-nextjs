259b92d2d0f54942997046cdde418cd6ea012d23
Eduardo Mangeli <eduardo@mangeli.com.br>
2021-09-06 12:03:37 -0300

    incluindo linkExterno como dado dos eventos

--- src/core/eventos.js
+++ src/core/eventos.js
@@ -18,6 +18,7 @@ export async function getAll(quant, search) {
             acf_data_evento {
               dataDoEvento
               tipo
+              linkExterno
             }
           }
         }
