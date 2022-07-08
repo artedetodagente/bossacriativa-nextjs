cf4bad791786b614fc9bd132b4cf8ff1b3efcd77
rlima-ferreira <renanferreiradelima98@live.com>
2022-07-08 13:49:12 -0300

    Remoção de links da SchoolBar e do Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,14 +4,15 @@ import React from 'react';
 import { Container } from './styles';
 
 export default function Footer() {
+
   const sponsors = [
     { image: require('@/images/footer-ufrj.svg'), url: '/' },
     { image: require('@/images/footer-emufrj.svg'), url: '/' },
     { image: require('@/images/footer-fujb.svg'), url: '/' },
     { image: require('@/images/footer-funarte.svg'), url: '/' },
-    { image: require('@/images/footer-mtur.svg'), url: '/' },
-    { image: require('@/images/footer-secult.svg'), url: '/' },
-    { image: require('@/images/footer-brasil.svg'), url: '/' },
+    // { image: require('@/images/footer-mtur.svg'), url: '/' },
+    // { image: require('@/images/footer-secult.svg'), url: '/' },
+    // { image: require('@/images/footer-brasil.svg'), url: '/' },
   ];
 
   return (
@@ -19,25 +20,23 @@ export default function Footer() {
       <div>
         <h4>Realização</h4>
         <ul>
-          {
-            sponsors.map((item, index) => (
-              <li key={index}>
-                <Link href={item.url}>
-                  <a>
-                    <div>
-                      <Image
-                        src={item.image}
-                        // width="180px"
-                        // height="80px"
-                        layout="fill"
-                        objectFit="contain"
-                      />
-                    </div>
-                  </a>
-                </Link>
-              </li>
-            ))
-          }
+          {sponsors.map((item, index) => (
+            <li key={index}>
+              <Link href={item.url}>
+                <a>
+                  <div>
+                    <Image
+                      src={item.image}
+                      // width="180px"
+                      // height="80px"
+                      layout="fill"
+                      objectFit="contain"
+                    />
+                  </div>
+                </a>
+              </Link>
+            </li>
+          ))}
           {/* <li /> */}
         </ul>
       </div>
--- src/components/SchoolBar/index.jsx
+++ src/components/SchoolBar/index.jsx
@@ -1,35 +1,31 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
 import Link from 'next/link';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { Bar } from './styles';
 
 export default function SchoolBar({ data }) {
-  const orgs = [
-    { name: 'UFRJ', url: '/' },
-    { name: 'Escola de Música da UFRJ', url: '/' },
-    { name: 'FUJB', url: '/' },
-    { name: 'FUNARTE', url: '/' },
-    { name: 'Cultura', url: '/' },
-    { name: 'Turismo', url: '/' },
-    { name: 'Governo Federal', url: '/' },
-  ];
+  // const orgs = [
+  //   { name: 'UFRJ', url: '/' },
+  //   { name: 'Escola de Música da UFRJ', url: '/' },
+  //   { name: 'FUJB', url: '/' },
+  //   { name: 'FUNARTE', url: '/' },
+  //   // { name: 'Cultura', url: '/' },
+  //   // { name: 'Turismo', url: '/' },
+  //   // { name: 'Governo Federal', url: '/' },
+  // ];
 
   return (
     <Bar>
-      {
-        data.map((item, index) => (
-          <Fragment key={index}>
-            <li key={item.id}>
-              <Link href={item.acf_links_topo?.urlTopo || '/'}>
-                <a target="_blank">
-                  {item.title}
-                </a>
-              </Link>
-            </li>
-            <li className="separador">|</li>
-          </Fragment>
-        ))
-      }
+      {data.map((item, index) => (
+        <React.Fragment key={index}>
+          <li key={item.id}>
+            <Link href={item.acf_links_topo?.urlTopo || '/'}>
+              <a target="_blank">{item.title}</a>
+            </Link>
+          </li>
+          <li className="separador">|</li>
+        </React.Fragment>
+      ))}
     </Bar>
   );
 }
