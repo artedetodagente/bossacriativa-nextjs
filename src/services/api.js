036d539b7d48100660d5f0ac8e6680236524c132
rlima-ferreira <renanferreiradelima98@live.com>
2022-08-05 11:58:00 -0300

    Mudança na url da api para o novo servidor

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ const API_URL = process.env.WORDPRESS_API_URL;
 export async function fetchAPI(query, { variables } = {}) {
   const headers = { 'Content-Type': 'application/json' };
 
-  const res = await fetch('https://wp-admin.bossacriativa.art.br/graphql', {
+  const res = await fetch('https://wp-admin.bossacriativa.artedetodagente.com.br/graphql', {
     method: 'POST',
     headers,
     body: JSON.stringify({
